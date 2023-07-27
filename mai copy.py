924fd62837020ee59f16277dd6f344ab9332c23b
Chirag Vaghasiya <chirag.vaghasiya@aspiresoftserv.in>
2023-07-27 18:17:40 +0530

    5354

--- mai copy.py
+++ mai copy.py
@@ -41,3 +41,40 @@ for emp in self.env['hr.employee'].search([('id', '=', 70)]):
         else:
             self.env['attendance.history'].create(data)
         # montyly_attendance_obj.with_context(allow_modify_confirmed_sheet=True).unlink()
+
+
+
+
+
+
+
+        #        counter_mounths = 4
+        # counter_break = 0
+
+        # for emp in self.env['hr.employee'].search([]):
+        #     while(True):
+        #         date_start = date.today() - relativedelta(months=counter_mounths,day=1)
+        #         date_end = date.today() - relativedelta(months=counter_mounths,day=31)
+        #         monthly_attendance = self.env['hr.attendance'].search([('employee_id', '=', emp.id), ('check_in', '>=', date_start), ('check_out', '<=', date_end)])
+        #         if not monthly_attendance:
+        #             counter_break += 1
+        #             if counter_break == 2:
+        #                 counter_break = 0
+        #                 counter_mounths = 4
+        #                 break
+        #             else:
+        #                 counter_mounths += 1
+        #                 continue
+        #         counter_mounths += 1
+        #         total_hours = sum(monthly_attendance.mapped('worked_hours'))
+        #         data = {
+        #             'start_date': date_start,
+        #             'end_date': date_end,
+        #             'total_hours': round(total_hours, 2),
+        #             'employee_id': emp.id
+        #         }
+        #         if self.env['attendance.history'].search([('employee_id', '=', emp.id), ('start_date', '=', date_start), ('end_date', '=', date_end)]):
+        #             continue
+        #         else:
+        #             self.env['attendance.history'].create(data)
+            # self.env['hr.attendance'].sudo().search([('employee_id', '=', emp.id), ('check_out', '<=', date.today() - relativedelta(months=3,day=31))]).unlink()
