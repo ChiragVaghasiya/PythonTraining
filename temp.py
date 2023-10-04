17ca79c314dd7e572e8cc877a51a9767e3335ad6
Chirag Vaghasiya <chirag.vaghasiya@aspiresoftserv.in>
2023-10-04 11:15:53 +0530

    eefe

---
+++ temp.py
@@ -0,0 +1,19 @@
+def calculate_sum(a, b):
+    total = a + b
+    return total
+
+def calculate_product(a, b):
+    product = a * b
+    return product
+
+def main():
+    num1 = 10
+    num2 = 5
+
+    sum_result = calculate_sum(num1, num2)
+    product_result = calculate_product(num1, num2)
+
+    print(f"Sum: {sum_result}")
+    print(f"Product: {product_result}")
+
+    main()
